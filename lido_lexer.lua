47879f489581a273b6d394b6158337f1bc6aa28a
Word30210 <wordisnottoto@gmail.com>
2023-05-29 20:03:32 +0900

    2023-05-29 20:03

--- lido_lexer.lua
+++ lido_lexer.lua
@@ -15,11 +15,15 @@ function lido_lexer.tokenization(line)
                 i = i + 1
             end
         elseif char == "." then
-            stack = char
-            i = i + 1
+            while i <= #line and line[i] == "." do
+                stack = stack.."."
+                i = i + 1
+            end
         elseif char == "," then
-            stack = char
-            i = i + 1
+            while i <= #line and line[i] == "," do
+                stack = stack..","
+                i = i + 1
+            end
         elseif char == "*" then
             while i <= #line and line[i] == "*" do
                 stack = stack.."*"
@@ -35,42 +39,14 @@ function lido_lexer.tokenization(line)
                 stack = stack.." "
                 i = i + 1
             end
-        elseif char == "\n" then
-            stack = char
+        elseif char == "\n" or char == "|" then
+            stack = "|"
             i = i + 1
         end
         table.insert(t, stack)
         stack = ""
     end
-
-    i = 1
-    local rt = {}
-    while i <= #t do
-        local s = t[i]
-        local token = ""
-        if s == " " then
-            token = "00"
-        elseif s == "/" then
-            token = "01"
-        elseif s == "\\" then
-            token = "02"
-        elseif s == "//" then
-            token = "03"
-        elseif s == "." then
-            token = "04"
-        elseif s == "," then
-            token = "05"
-        elseif s == "*" then
-            token = "06"
-        elseif s == "%" then
-            token = "07"
-        elseif s == "\n" then
-            token = "ff"
-        end
-        table.insert(rt, token)
-        i = i + 1
-    end
-    return rt
+    return t
 end
 
 splitM.printT(lido_lexer.tokenization("// ... * ,,,\n// .. % ."))
